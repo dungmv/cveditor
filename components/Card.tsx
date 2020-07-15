fed5dee623894fbe1d97b468ce6411ca9cedecc0
Admin <admin@localhost.com>
2020-07-15 17:01:27 +0700

    add editable

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
-import { useDrag, useDrop } from 'react-dnd'
+import { useDrag, useDrop, DropTargetMonitor } from 'react-dnd'
 import { ItemTypes } from './ItemTypes'
+import { XYCoord } from 'dnd-core'
 
 import { H5Editable, PEditable, AEditable } from './index.js'
 
@@ -11,42 +12,65 @@ const style = {
     backgroundColor: 'white',
     cursor: 'move',
 }
-export const Card = ({ id, text, index, moveCard }) => {
-    const ref = useRef(null)
+
+export interface CardProps {
+    id: any
+    text: string
+    index: number
+    moveCard: (dragIndex: number, hoverIndex: number) => void
+}
+
+interface DragItem {
+    index: number
+    id: string
+    type: string
+}
+export const Card: React.FC<CardProps> = ({ id, text, index, moveCard }) => {
+    const ref = useRef<HTMLDivElement>(null)
     const [, drop] = useDrop({
         accept: ItemTypes.CARD,
-        hover(item, monitor) {
+        hover(item: DragItem, monitor: DropTargetMonitor) {
             if (!ref.current) {
                 return
             }
             const dragIndex = item.index
             const hoverIndex = index
+
             // Don't replace items with themselves
             if (dragIndex === hoverIndex) {
                 return
             }
+
             // Determine rectangle on screen
             const hoverBoundingRect = ref.current?.getBoundingClientRect()
+
             // Get vertical middle
             const hoverMiddleY =
                 (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
             // Determine mouse position
             const clientOffset = monitor.getClientOffset()
+
             // Get pixels to the top
-            const hoverClientY = clientOffset.y - hoverBoundingRect.top
+            const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+
             // Only perform the move when the mouse has crossed half of the items height
             // When dragging downwards, only move when the cursor is below 50%
             // When dragging upwards, only move when the cursor is above 50%
+
             // Dragging downwards
             if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
                 return
             }
+
             // Dragging upwards
             if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
                 return
             }
+
             // Time to actually perform the action
             moveCard(dragIndex, hoverIndex)
+
             // Note: we're mutating the monitor item here!
             // Generally it's better to avoid mutations,
             // but it's good here for the sake of performance
@@ -54,12 +78,14 @@ export const Card = ({ id, text, index, moveCard }) => {
             item.index = hoverIndex
         },
     })
+
     const [{ isDragging }, drag] = useDrag({
         item: { type: ItemTypes.CARD, id, index },
-        collect: (monitor) => ({
+        collect: (monitor: any) => ({
             isDragging: monitor.isDragging(),
         }),
     })
+
     const opacity = isDragging ? 0 : 1
     drag(drop(ref))
     return (
