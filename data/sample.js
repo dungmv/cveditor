31353f330dfe6deddec536c25c49c53ffc066d07
Admin <admin@localhost.com>
2020-07-18 10:06:28 +0700

    add data sample

---
+++ data/sample.js
@@ -0,0 +1,137 @@
+export default {
+    name: 'SIMON DOE',
+    position: 'Senior Software Engineer',
+    summary: 'Summarise your career here. You can make a PDF version of your resume using our free Sketch template here. Donec quam felis, ultricies nec, pellentesque eu. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.',
+    phone: '0123 4567 890',
+    email: 'simon.doe@yourwebsite.com',
+    site: 'www.yourwebsite.com',
+    address: 'New York',
+    experiences: [
+        {
+            start: '2019',
+            end: 'Present',
+            company: 'Google',
+            position: 'Senior Software Engineer',
+            description: '<p>Role description goes here ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Donec pede justo, fringilla vel. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis</p><ul><li>Lorem ipsum dolor sit amet, consectetuer.</li><li>Aenean commodo ligula eget dolor.</li><li>Etiam ultricies nisi vel augue.</li></ul>'
+        },
+        {
+            start: '2016',
+            end: '2019',
+            company: 'Apple',
+            position: 'Lead Software Developer',
+            description: '<p>Role description goes here ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Donec pede justo, fringilla vel</p><ul><li>Lorem ipsum dolor sit amet, consectetuer.</li><li>Aenean commodo ligula eget dolor.</li></ul>'
+        },
+        {
+            start: '2014',
+            end: '2016',
+            company: 'Dropbox',
+            position: 'Senior Software Developer',
+            description: '<p>Role description goes here ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus</p>'
+        },
+        {
+            start: '2013',
+            end: '2014',
+            company: 'Uber',
+            position: 'Senior Developer',
+            description: 'Role description goes here ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus'
+        },
+        {
+            start: '2016',
+            end: '2019',
+            company: 'Amazon',
+            position: 'Lead Software Developer',
+            description: 'Role description goes here ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus'
+        },
+        {
+            start: '2013',
+            end: '2014',
+            company: 'Startup',
+            position: 'Frontend Developer',
+            description: 'Role description goes here ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus'
+        }
+    ],
+    projects: [
+        {
+            name: 'Project Lorem Ipsum',
+            start: '',
+            end: '',
+            type: 'Open Source',
+            description: 'You can use this section for your side projects. You can provide a project link here as well. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus'
+        },
+        {
+            name: 'Project Sed Fringilla',
+            start: '',
+            end: '',
+            type: 'Open Source',
+            description: 'You can use this section for your side projects. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim'
+        },
+        {
+            name: 'Project Praesent',
+            start: '',
+            end: '',
+            type: 'Open Source',
+            description: 'You can use this section for your side projects. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim'
+        }
+    ],
+    education: [
+        {
+            title: 'MSc in Computer Science',
+            start: '2010',
+            end: '2011',
+            description: 'University College London'
+        },
+        {
+            title: 'BSc Maths and Physics',
+            start: '2007',
+            end: '2010',
+            description: 'Imperial College London'
+        }
+    ],
+    awards: [
+        {
+            name: 'Award Lorem Ipsum',
+            date: '2019',
+            organization: 'Microsoft lorem ipsum'
+        },
+        {
+            name: 'Award Donec Sodales',
+            date: '2017',
+            organization: 'Oracle Aenean'
+        }
+    ],
+    languages: [
+        {
+            name: 'English',
+            level: 'Native'
+        },
+        {
+            name: 'Spanish',
+            level: 'Professional'
+        }
+    ],
+    interests: [
+        'Climbing',
+        'Snowboarding',
+        'Photography',
+        'Travelling'
+    ],
+    skills: [{
+        title: 'Technical',
+        items: [
+            'JavaScript/Angular/React/Vue',
+            'Python/Ruby/PHP',
+            'Node.js/ASP.NET',
+            'PostgreSQL/MySQL',
+            'Object-oriented design',
+            'Design and implement database structures',
+        ]
+    }, {
+        title: 'Professional',
+        items: [
+            'Effective communication',
+            'Team player',
+            'Strong problem solver',
+            'Good time management'
+        ]
+    }]
+}
