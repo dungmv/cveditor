477ab070e10941ae5e67d83989ea6900317166cd
Admin <admin@localhost.com>
2020-07-20 10:44:02 +0700

    add jsx parser

--- pages/cvex.tsx
+++ pages/cvex.tsx
@@ -1,18 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { DndProvider } from 'react-dnd'
+import JsxParser from 'react-jsx-parser'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-export default ({data}) => {
+export default ({ data }) => {
     const [template, setTemplate] = useState<string>(null);
     useEffect(() => {
         fetch('/api/templates')
-        .then(res => res.text())
-        .then(text => setTemplate(text))
+            .then(res => res.text())
+            .then(text => setTemplate(text))
     });
     return (
         <div className="container px-3 px-lg-5">
             <DndProvider backend={HTML5Backend}>
-                {template? <div dangerouslySetInnerHTML={{__html: template}}></div>:null}
+                <JsxParser
+                    bindings={{}}
+                    components={{}}
+                    jsx={template}
+                />
+                {/* {template? <div dangerouslySetInnerHTML={{__html: template}}></div>:null} */}
             </DndProvider>
         </div>
     )
