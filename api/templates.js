2f316ae90d77644eb77e76855862995d379761fb
Admin <admin@localhost.com>
2020-08-25 09:51:34 +0700

    query cv template from db

--- api/templates.js
+++ api/templates.js
@@ -4,36 +4,19 @@ const AdmZip = require('adm-zip');
 const parser = require('./cv-parser');
 const Template = require('../databases/models/Template');
 
-const readSection = (section, childCount) => {
-    const jsx = fs.readFileSync(`./assets/src/sec-${section}.html`, 'utf8');
-    const subs = [];
-    for (let i = 0; i < childCount; i++) {
-        subs.push(fs.readFileSync(`./assets/src/sub-${section}-${i}.html`, 'utf8'))
+const get = async (req, res) => {
+    templateId = parseInt(req.params.id);
+    const template = await Template.findOne({where: {id: templateId}});
+    if (!template) {
+        res.statusCode = 404;
+        res.json({err: 404, msg: 'template not found'});
+        return
     }
-    return { jsx, subs }
-}
-
-const get = (req, res) => {
-    const jsx = fs.readFileSync('./assets/src/index.html', 'utf8');
-    const header = fs.readFileSync('./assets/src/header.html', 'utf8');
-    const footer = fs.readFileSync('./assets/src/footer.html', 'utf8');
-    const s11 = readSection('0-0', 6);
-    const s12 = readSection('0-1', 3);
-    const s21 = readSection('1-0', 2);
-    const s22 = readSection('1-1', 2);
-    const s23 = readSection('1-2', 2);
-    const s24 = readSection('1-3', 2);
-    const s25 = readSection('1-4', 0);
-    res.statusCode = 200;
-    res.json({
-        jsx,
-        header,
-        footer,
-        sections: [
-            [s11, s12],
-            [s21, s22, s23, s24, s25],
-        ]
-    });
+    zlib.inflate(template.data, (err, result) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(result);
+    })
 }
 
 const create = (req, res) => {
